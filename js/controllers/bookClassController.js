4f4e012eae852d0200ad4612374acf06961d3721
annadowling <anna.dowling@errigal.com>
2016-03-20 21:25:39 +0000

    Adding fixed height for dashboard page

--- js/controllers/bookClassController.js
+++ js/controllers/bookClassController.js
@@ -1,99 +1,87 @@
+ fitFocusApp.controller('bookClassController', '$scope', function($scope) {
+
+     $scope.days = [
+         {
+         monday: {
+             name: 'Monday',
+             slots: {
+                 900: {
+                     time: '9:00am',
+                     booked: false
+                 },
+                 110: {
+                     time: '11:00am',
+                     booked: false
+                 }
+             }
+         },
+         tuesday: {
+             name: 'Tuesday',
+             slots: {
+                 900: {
+                     time: '9:00am',
+                     booked: false
+                 },
+                 110: {
+                     time: '11:00am',
+                     booked: false
+                 }
+             }
+         },
+         wednesday: {
+             name: 'Wednesday',
+             slots: {
+                 900: {
+                     time: '9:00am',
+                     booked: false
+                 },
+                 110: {
+                     time: '11:00am',
+                     booked: false
+                 }
+             }
+         },
+         thursday: {
+             name: 'Thursday',
+             slots: {
+                 900: {
+                     time: '9:00am',
+                     booked: false
+                 },
+                 110: {
+                     time: '11:00am',
+                     booked: false
+                 }
+             }
+         },
+         friday: {
+             name: 'Friday',
+             slots: {
+                 900: {
+                     time: '9:00am',
+                     booked: false
+                 },
+                 110: {
+                     time: '11:00am',
+                     booked: false
+                 }
+             }
+         },
+         satuday: {
+             name: 'Satuday',
+             slots: {
+                 900: {
+                     time: '9:00am',
+                     booked: false
+                 },
+                 110: {
+                     time: '11:00am',
+                     booked: false
+                 }
+             }
+         }
+     }];
+ });
 
 
-    fitFocusApp.controller('bookClassController', function($scope) {
-        $scope.reset = function() {
-
-            $scope.days = [({
-                monday: {
-                    name: 'Monday',
-                    slots: {
-                        900: {
-                            time: '9:00am',
-                            booked: false
-                        },
-                        0110: {
-                            time: '11:00am',
-                            booked: false
-                        },
-                        100: {
-                            time: '1:00pm',
-                            booked: false
-                        },
-                        300: {
-                            time: '3:00pm',
-                            booked: false
-                        },
-                        500: {
-                            time: '5:00pm',
-                            booked: false
-                        },
-                        700: {
-                            time: '7:00pm',
-                            booked: false
-                        }
-                    }
-                },
-                tuesday: {
-                    name: 'Tuesday',
-                    slots: {
-                        900: {
-                            time: '9:00am',
-                            booked: false
-                        },
-                        0110: {
-                            time: '11:00am',
-                            booked: false
-                        },
-                        100: {
-                            time: '1:00pm',
-                            booked: false
-                        },
-                        300: {
-                            time: '3:00pm',
-                            booked: false
-                        },
-                        500: {
-                            time: '5:00pm',
-                            booked: false
-                        },
-                        700: {
-                            time: '7:00pm',
-                            booked: false
-                        }
-                    }
-                },
-                wednesday: {
-                    name: 'Wednesday',
-                    slots: {
-                        900: {
-                            time: '9:00am',
-                            booked: false
-                        },
-                        0110: {
-                            time: '11:00am',
-                            booked: false
-                        },
-                        100: {
-                            time: '1:00pm',
-                            booked: false
-                        },
-                        300: {
-                            time: '3:00pm',
-                            booked: false
-                        },
-                        500: {
-                            time: '5:00pm',
-                            booked: false
-                        },
-                        700: {
-                            time: '7:00pm',
-                            booked: false
-                        }
-                    }
-                }
-            })];
-
-        };
-
-    });
 
